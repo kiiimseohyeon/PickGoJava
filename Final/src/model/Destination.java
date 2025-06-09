c7dcd03ac04519cf6b96a1df2c647b47776b9acd
kiiimseohyeon <106173332+kiiimseohyeon@users.noreply.github.com>
2025-06-10 01:10:02 +0900

    Update Destination.java

--- Final/src/model/Destination.java
+++ Final/src/model/Destination.java
@@ -3,10 +3,23 @@ package model;
 public class Destination {
     private final String name;
     private final String category;
+    private final String address;
+    private final String description;
 
+    // 목록 출력용
     public Destination(String name, String category) {
         this.name = name;
         this.category = category;
+        this.address = null;
+        this.description = null;
+    }
+
+    // 상세 정보용
+    public Destination(String name, String category, String address, String description) {
+        this.name = name;
+        this.category = category;
+        this.address = address;
+        this.description = description;
     }
 
     public String getName() {
@@ -15,5 +28,13 @@ public class Destination {
 
     public String getCategory() {
         return category;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
