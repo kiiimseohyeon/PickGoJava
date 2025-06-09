1680b41f5887815907a6b7a64123761690485a91
kiiimseohyeon <106173332+kiiimseohyeon@users.noreply.github.com>
2025-06-09 17:08:53 +0900

    Update Destination.java

--- Final/src/model/Destination.java
+++ Final/src/model/Destination.java
@@ -1,1 +1,19 @@
+package model;
 
+public class Destination {
+    private final String name;
+    private final String category;
+
+    public Destination(String name, String category) {
+        this.name = name;
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+}
